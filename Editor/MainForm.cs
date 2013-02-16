7325aee1b9d3a5ad9ab1893643969ef0772ae167
Austin Murphy <Borzen00@gmail.com>
2013-02-16 14:41:39 -0600

    Figured out how to grab the chunks.

--- Editor/MainForm.cs
+++ Editor/MainForm.cs
@@ -92,8 +92,9 @@ namespace AweEditor
         /// </summary>
         private void ImportVoxelTerrainMenuClicked(object sender, EventArgs e)
         {
+            int deflateMax = 1024 * 64;
             MinecraftChunk[] mc = new MinecraftChunk[1024];
-
+            int mcNum = 0;
             int[] chunkLocation = new int[1024];
 
             int[] chunkSize = new int[1024];
@@ -117,47 +118,61 @@ namespace AweEditor
                                 "All Files (*.*)|*.*";
             if (fileDialog.ShowDialog() == DialogResult.OK)
             {
+                int localX = 0, localZ = 0;
+                int offset = 0;
+                int sectorNumber = 0;
+                int chunkL = 0;
+                char[] delm = { '\\', '.' };
+                string[] fn = fileDialog.FileName.Split(delm);
+                for (int i = 0; i < fn.Length; i++)
+                {
+                    if (fn[i] == "r")
+                    {
+                        localX = Convert.ToInt32(fn[i+1]);
+                        localZ = Convert.ToInt32(fn[i+2]);
+                        break;
+                    }
+                }
                 FileStream f = new FileStream(fileDialog.FileName, FileMode.Open, FileAccess.Read);
                 f.Flush();
-                byte[] bi= new byte[(int)f.Length];
-                f.Read(bi,0,(int)f.Length);
-                byte[] biH = new byte[8192];
-                byte[] biE = new byte[bi.Length - 8192];
-                int j = 0;
-                int k = 0;
-                for (int i = 0; i < 8192; i++)
-                {
-                    biH[i] = bi[j];
-                    j++;
-                }
-                int localX = 0 >> 5;
-                int localZ = 0 >> 5;
-                for (int i = 0; i < biE.Length; i++)
-                {
-                    biE[i] = bi[j];
-                    j++;
-                }
-                j = 0;
-                while (k < 4096)
-                {
-                    chunkLocation[j] = biH[k] << 16 | biH[k + 1] << 8 | biH[k + 2];
-                    chunkSize[j] = biH[k + 3];
-                    k += 4;
-                    j++;
-                }
-                j = 0;
-                k = 0;
-                
-                while (k < 1024)
-                {
-                    if (chunkSize[k] == 0)
+                byte[] hedder = new byte[8 * 1024];
+                f.Read(hedder, 0, 8 * 1024);
+                int chunkStart = 4 * ((localX & 31) + (localZ & 31) * 32);
+                while(chunkStart < 4096){
+                    f.Flush();
+                    byte[] buffer = new byte[5];
+                    f.Seek(chunkStart, SeekOrigin.Begin);
+                    f.Read(buffer, 0, 4);
+                    sectorNumber = buffer[3];
+                    offset = buffer[0] << 16 | buffer[1] << 8 | buffer[2];
+                    chunkStart += 4;
+                    if (offset != 0)
                     {
-                        k++;
-                    }
-                    else
-                    {
-                        j = chunkLocation[k];
-                        int cL = biE[j] << 24 | biE[j + 1] << 16 | biE[j + 2] << 8 | biE[j + 3];
+                        f.Seek(4096 * offset, SeekOrigin.Begin);
+                        f.Read(buffer, 0, 5);
+                        chunkL = buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3];
+                        byte[] chunk = new byte[chunkL - 1];
+                        f.Read(chunk, 0, chunkL - 1);
+                        int j = 0;
+                        byte[] chunkD = new byte[chunk.Length-6];
+                        for (int i = 0; i < chunk.Length - 4; i++)
+                        {
+                            if (i != 0)
+                            {
+                                if (i != 1)
+                                {
+                                    chunkD[j] = chunk[i];
+                                    j++;
+                                }
+                            }
+                        }
+                        DeflateStream dfs = new DeflateStream(new MemoryStream(chunkD), CompressionMode.Decompress);
+                        byte[] dChunk = new byte[chunkD.Length];
+                        dfs.Flush();
+                        dfs.Read(dChunk, 0, chunkD.Length);
+                        mc[mcNum] = new MinecraftChunk(dChunk);
+                        mcNum++;
+                        dfs.Close();
                     }
                 }
                 f.Close();
