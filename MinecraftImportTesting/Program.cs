76403682723e427d830efe832fc30305c87fbf91
Devin Kelly-Collins <dkellycollins@gmail.com>
2013-02-18 14:14:31 -0600

    Working...

---
+++ MinecraftImportTesting/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MinecraftImportTesting
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            fileDialog.InitialDirectory = ContentPath();
+
+            fileDialog.Title = "Select File";
+
+            fileDialog.Filter = "All Files (*.*)|*.*";
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ProcessMinecraftFile(fileDialog.FileName, Path.ChangeExtension(fileDialog.FileName, ".out"));
+            }
+        }
+
+        public static void ProcessMinecraftFile(string input, string output)
+        {
+            //First Error checking
+            if (!File.Exists(input))
+                throw new ArgumentException("Input file must be a minecraft map file.");
+
+            //First lets just throw out the bits
+            Stream inputStream = File.OpenRead(input);
+            Stream outputStream = File.Create(output);
+
+            int buffer = 0;
+            while ((buffer = inputStream.ReadByte()) != -1)
+            {
+                outputStream.WriteByte((byte)buffer);
+            }
+        }
+
+        private static string ContentPath()
+        {
+            // Default to the directory which contains our content files.
+            string assemblyLocation = Assembly.GetExecutingAssembly().Location;
+            string relativePath = Path.Combine(assemblyLocation, "../../../../Content");
+            string contentPath = Path.GetFullPath(relativePath);
+            return contentPath;
+        }
+    }
+}
---
+++ MinecraftImportTesting/VoxelTerrain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinecraftImportTesting
+{
+    /// <summary>
+    /// BlockType corresponds to Minecraft Bock IDs for ease of import
+    /// (see http://www.minecraftwiki.net/wiki/Data_values#Block_IDs)
+    /// </summary>
+    public enum BlockType
+    {
+        Air = 0,
+        Stone = 1,
+        Grass = 2,
+        Dirt = 3,
+        Water = 4,
+    }
+
+    /// <summary>
+    /// A class to represent voxel terrain
+    /// </summary>
+    public class VoxelTerrain
+    {
+        private BlockType[][][] _terrain;
+    }
+}
